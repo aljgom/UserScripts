64ab250125c6ed23cbeb51622f0eb2cf0a0ba548
Alejandro Gomez <aljgom@gmail.com>
2018-09-25 14:57:27 -0500

    added match to keep.js

--- keep.js
+++ keep.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name         Keep
+// @name         Keep Backup
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Selects google keep to back it up
+// @description  -
 // @author       aljgom
+// @match        https://takeout.google.com/settings/takeout
 // @match        https://takeout.google.com/settings/takeout/
 // @grant        none
 // ==/UserScript==
@@ -15,9 +16,10 @@ console.log(GM_info.script.name+"%c Script: "+document.location.toString(),'colo
     var sleep = ms => new Promise(resolve=>setTimeout(resolve,ms))
 
     await sleep(2000);
-    document.getElementsByClassName('RveJvd snByac')[2].click();            // click on select none
+    document.getElementsByClassName('RveJvd snByac')[2].click();          // click on select none
 
-    // Find the toogle for Keep by reading all the app names.
+
+    // Find the toogle for Keep by reading all the names.
     var keep = null;
     HTMLCollection.prototype.forEach = Array.prototype.forEach;
     document.getElementsByClassName('uz1rMe').forEach(el=>{
@@ -28,11 +30,11 @@ console.log(GM_info.script.name+"%c Script: "+document.location.toString(),'colo
         }
     })
 
-    document.getElementsByClassName('E7QdY espmsb')[61].scrollIntoView();   // scroll to element before keep because keep is covered by bar
-    await sleep(2000);
-    keep.click();                                                          // select keep
 
-    // click on 'create' button after 'selected' button is click
+    document.getElementsByClassName('E7QdY espmsb')[61].scrollIntoView(); // scroll to element before keep because keep is covered by bar
+    await sleep(2000);
+    keep.click();
+
     document.getElementsByClassName('RveJvd snByac')[20].addEventListener('click', async()=>{
 	    await sleep(1000);
 	    document.getElementsByClassName('RveJvd snByac')[22].click();
