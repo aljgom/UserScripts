3b69311e616a3cf97edb1083c6bdfd2af3d83a04
Alejandro Gomez <aljgom@gmail.com>
2018-10-05 00:21:22 -0500

    Google Sheet Scroll: removed script identification, import separate file for it instead

--- google_sheets_scroll.js
+++ google_sheets_scroll.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name         5x5
+// @name         Google Sheets Scroll
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  -
+// @description  Scroll to certain position when google sheet loads
 // @author       aljgom
 // @match        https://docs.google.com/spreadsheets/d/1vctvEcsYrLRACOhMfd9ThbRTbVTmZGwD9p5qIcQNBwQ/*
 // @grant        none
 // ==/UserScript==
-console.log(GM_info.script.name+"%c Script: "+document.location.toString(),'color:green');
+
 (async function() {
     'use strict';
 
 
     var [yBar, xBar] = await waitFor("document.querySelectorAll('.native-scrollbar')", 30*1000);
-    confirm();
+    confirm();  // To bring focus to the page once loaded
     yBar.scrollTop = 1800;
     xBar.scrollLeft = Math.floor( (Date.now() - (new Date('3/31/2018')) )/(1000*60*60*24) ) * 100 + 5000;  // 17xxx days, each cell is 100 width, scroll to 5000 + days*100 ;
 
