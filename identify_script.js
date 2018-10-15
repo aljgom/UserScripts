c2897a6185f499365b1770bea017f704e8052c34
Alejandro Gomez <aljgom@gmail.com>
2018-10-15 01:36:57 -0500

    Identify script: updated description

--- identify_script.js
+++ identify_script.js
@@ -1,2 +1,4 @@
-  // log script name and url it's running on
-  console.log(GM_info.script.name+"%c Script: "+document.location.toString(),'color:green');        
+// This code can be imported into any script using @require,
+// helpful to add to all scripts to easily identify when a script is running in a page, and all the other pages (iframes) that trigger script runs
+// Logs the script's name and the url it's running on
+console.log(GM_info.script.name+"%c Script: "+document.location.toString(),'color:green');
