6ddcc1e82743fc2048f6a7b681565da99581c2f1
Alejandro Gomez <aljgom@gmail.com>
2018-11-16 16:49:21 -0500

    renamed Google Doc Mirror

--- google_doc_mirror.user.js
+++ google_doc_mirror.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Mirror Google Doc
+// @name         Google Doc Mirror
 // @namespace    aljgom
 // @version      0.1
 // @description  Mirrors the content of the google doc in a new window. If the window is clicked the whole content is selected so it can be copied easily
@@ -19,23 +19,24 @@
     }
 
     if(window.newWin) newWin.close();
-    clearInterval(window.copy_inter)
-    newWin = window.open('','','width=700, height=700, top=1000, left =100');
+    clearInterval(window.mirror_inter)
+    newWin = window.open('','','width=700, height=700, top=1000, left =100');       // global scope
     await sleep(2000) // onload won't work because it's an empty page
+    var tarea = newWin.document.createElement('textarea');
     newWin.document.title = 'Google Doc Mirror';
-    newWin.document.body.append( tarea = newWin.document.createElement('textarea') );
+    newWin.document.body.append(tarea);
     tarea.style.width = tarea.style.height = "100%";
 	tarea.onclick= function(){ this.select() } // automatically select all text when textarea is clicked
 
-    mirror_inter = setInterval(function(){
-        all = '';
+    mirror_inter = setInterval(function(){                                           // global scope
+        var all = '';
         $('.kix-lineview-content').each(function(){
             // add line
             $('.kix-lineview-text-block',this).each(function(){
-                spaces = parseInt($(this).css('padding-left') )/6;  // calculate the number of spaces from css
+                var spaces = parseInt($(this).css('padding-left') )/6;  // calculate the number of spaces from css
                 all +=  " ".repeat(spaces) + $(this).text().replace(/​/g,"").replace(/ /g,"");
-	            });
-            	all += "\n" ;
+            });
+            all += "\n" ;
          })
         tarea.value = all
     },200)
