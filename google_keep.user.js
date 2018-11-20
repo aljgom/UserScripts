52a0c217800a35cc7bf2e07f715d39ac2e9f9a50
Alejandro Gomez <aljgom@gmail.com>
2018-11-20 01:41:06 -0500

    Google Keep: changing titles now works in German too

--- google_keep.user.js
+++ google_keep.user.js
@@ -14,6 +14,7 @@
     setInterval(()=>{
         if(document.location.href.match('#home')) return;           // Leave the homepage alone
         var titles = document.querySelectorAll('[aria-label=Title]')
+        if(titles.length == 0) titles = document.querySelectorAll('[aria-label=Titel]') // Try it in German
         document.title = titles[titles.length-1].innerHTML          // Change the tab title to the name of the current List/Note
     },2000);
 
