a3bf7697872832633ed75e4b772d8474ca328283
Alejandro Gomez <aljgom@gmail.com>
2019-03-18 22:45:58 -0700

    Keep Backup: website was updated, updated selectors

--- google_keep_backup.user.js
+++ google_keep_backup.user.js
@@ -17,30 +17,30 @@
 
     if(url.match('https://takeout.google.com')){
         await sleep(1000);
-        document.getElementsByClassName('RveJvd snByac')[2].click();          // click on select none
+        document.getElementsByClassName('ksBjEc')[1].click();           // click on select none
 
 
         // Find the toogle for Keep by reading all the names.
-        var keep = null;
+        var keepBox = null;
         HTMLCollection.prototype.forEach = Array.prototype.forEach;
-        document.getElementsByClassName('uz1rMe').forEach(el=>{
-            if(el.children[2] && el.children[2].children[0]){
-                if(["Keep", "Notizen"].indexOf(el.children[2].children[0].innerText) >= 0){     // see if the name matches the list (Notizen = german Keep)
-                   keep = el.children[5].children[0].children[0];
-                }
-            }
-        });
+        document.getElementsByClassName('gGfIad').forEach(el=>{         // selector for whole row
+           if(el.children[1] && el.children[1].children[0]){            // make sure node has children
+               if(["Keep", "Notizen"].includes(el.children[1].children[0].innerText)){     // see if the name matches the list (Notizen = german Keep)
+                  keepBox = el.children[2].children[0].children[0];     // keep checkbox
+               }
+           }
+       });
 
         // Scroll to Google Keep toggle
-        keep.scrollIntoView();
-        window.scrollTo(0,window.scrollY-200);      // scroll up a bit because keep is covered by bar
+        keepBox.scrollIntoView();
+        window.scrollTo(0,window.scrollY-300);                          // scroll up a bit because keep is covered by bar
         await sleep(1000);
-        keep.click();
+        keepBox.click();
 
         // Add listener to 'select' button so that 'backup' button will be pressed automatically
-        document.getElementsByClassName('RveJvd snByac')[20].parentElement.parentElement.addEventListener('click', async ()=>{
+        document.getElementsByClassName('oFLoie')[0].children[0].addEventListener('click', async ()=>{
             await sleep(1000);
-            document.getElementsByClassName('RveJvd snByac')[22].click();
+            document.getElementsByClassName('oFLoie')[1].children[0].click();
         });
     }
 
