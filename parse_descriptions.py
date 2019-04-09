fe224e9ce14b11ff6d3a1c061ae8cfd01f9217c2
Alejandro Gomez <aljgom@gmail.com>
2019-04-08 17:17:01 -0700

    Added link to /old in readme

--- parse_descriptions.py
+++ parse_descriptions.py
@@ -73,6 +73,10 @@ def create_table(path):
     with open(path + '_preview_table.md', 'w') as output:
         output.write('| Name / File | Description / Match |\n')
         output.write('|---|---|\n')
+        if path == "./":
+            output.write('| [Old Userscripts](old/) | Older Userscripts | \n')
+        # else:
+        #     output.write('| fd[Main Userscripts](/../) | Return to main table | \n')
         for file_name in os.listdir(path):
             if "user.js" in file_name:
                 parse_file(path, file_name, output)
