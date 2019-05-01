5e09be2f4b192f41808d92875663d7fced761758
Alejandro Gomez <aljgom@gmail.com>
2019-05-01 16:42:11 -0700

    Google Sheets Scroll: changes so more pages can be scrolled

--- google_sheets_scroll.user.js
+++ google_sheets_scroll.user.js
@@ -4,20 +4,34 @@
 // @version      0.1
 // @description  Scroll to certain position when google sheet loads
 // @author       aljgom
-// @match        https://docs.google.com/spreadsheets/d/1vctvEcsYrLRACOhMfd9ThbRTbVTmZGwD9p5qIcQNBwQ/*
+// @match        https://docs.google.com/spreadsheets/d/*
 // @grant        none
 // ==/UserScript==
 
 (async function() {
     'use strict';
+    while(typeof(waitFor) === "undefined"){
+        console.log('waiting for All Pages')
+        await new Promise(resolve=>setInterval(resolve, 200))
+    }
 
+    let url = document.location.href;
+    // await waitFor(()=>document.querySelector(".jfk-butterBar-shown") == null)  // wait for 'still working' message to dissapear
+    let [yBar, xBar] = await waitFor(()=>document.querySelectorAll('.native-scrollbar'));
+    let cellHeight = 21;
+    let cellWidth = 100;
+    let daysSince = date =>   Math.floor( (Date.now() - (new Date(date)) )/(1000*60*60*24) );
 
-    var [yBar, xBar] = await waitFor("document.querySelectorAll('.native-scrollbar')", 30*1000);
-    confirm();  // To bring focus to the page once loaded
-    yBar.scrollTop = 1800;
-    xBar.scrollLeft = Math.floor( (Date.now() - (new Date('3/31/2018')) )/(1000*60*60*24) ) * 100 + 5000;  // 17xxx days, each cell is 100 width, scroll to 5000 + days*100 ;
+    if(url.match("https://docs.google.com/spreadsheets/d/1vctvEcsYrLRACOhMfd9ThbRTbVTmZGwD9p5qIcQNBwQ/")){
+        confirm();  // To bring focus to the page once loaded, the scroll buttons in the scrollbar need to be clicked once for it to scroll to the right position
+        yBar.scrollTop = 1800;
+        xBar.scrollLeft = daysSince('3/31/2018') * cellWidth + 5000;  // 17xxx days, scroll to 5000 + days*cellWidth ;
+    }
 
-
+    if(url.match("https://docs.google.com/spreadsheets/d/1Blx76YvGaXwllkD4FC0MKKT9PAVOlRWsE2br7s9rka0")){
+        //confirm();   // To bring focus to the page once loaded, the scroll buttons in the scrollbar need to be clicked once for it to scroll to the right position
+        yBar.scrollTop = (4260  + 24 * daysSince('4/1/2019')) * cellHeight;
+    }
 
 
 })();
