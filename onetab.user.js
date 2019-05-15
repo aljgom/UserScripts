cbe683ab96124fdbb57e2396743726c533add3ab
Alejandro Gomez <aljgom@gmail.com>
2019-05-15 07:58:44 -0700

    OneTab: open all links if url contains openAll=true

--- onetab.user.js
+++ onetab.user.js
@@ -2,20 +2,44 @@
 // @name         OneTab
 // @namespace    aljgom
 // @version      0.1
-// @description  additions to the OneTab 'share as website' page
+// @description  additions to the OneTab 'share as website' page. Prompting for tab titles, adding buttons for opening all links, opening all automatically if url contains openAll=true
 // @author       aljgom
 // @match        https://www.one-tab.com/page/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
 // @grant        GM_listValues
+// @grant        unsafewindow
+// @grant        window.close
 // ==/UserScript==
 //
 (async function() {
     'use strict';
-    let url = document.location.href;
+    while(typeof(waitFor) === "undefined"){
+        console.log('waiting for All Pages')
+        await new Promise(resolve=>setInterval(resolve, 200))
+    }
 
+    let url = document.location.href;
     await sleep(500);                    /* Wait for links to show up in page */
+    let links = document.querySelectorAll('body>div>div>a');
+    for(let link of links){
+        link.href = link.href.replace('http://chrome-extension','chrome-extension:');  /* replace for suspended tabs from The Great Suspender extension */
+    }
+
+    async function openAll(wait=false){
+        for(let link of links){
+            open(link.href);
+            if(wait) await sleep(3000);
+        }
+    }
+
+    // if the url contains openAll=true, it opens all the links automatically
+    if(urlParams.get('openAll') == 'true') {
+        openAll();
+        window.close();
+    }
+
     /* Change the Tab Title */
     // Look in the script's storage for a title
     // if it doesn't exist, prompt for one and store it
@@ -34,18 +58,6 @@
 
 
     /* Add 'open all links' buttons */
-    let links = document.querySelectorAll('body>div>div>a');
-    for(let link of links){
-        link.href = link.href.replace('http://chrome-extension','chrome-extension:');  /* replace for suspended tabs from The Great Suspender extension */
-    }
-
-    async function openAll(wait=false){
-        for(let link of links){
-            open(link.href);
-            if(wait) await sleep(3000);
-        }
-    }
-
     let button = document.createElement('div');
     button.innerHTML = `<a href='javascript:void(0)' id='openAll' style='font-size:12px'>Open all links</a>`
     document.body.children[0].children[3].appendChild(button)
