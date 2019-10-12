4d03129288b9b13327d319e0779622fe3c4360b5
Alejandro Gomez <aljgom@gmail.com>
2019-10-11 21:19:14 -0700

    Airbnb Multicalendar: Added colors for check-ins and check-outs for today. Fixed bug for check out dates. Using polling to check if the booking info changed instead of a hard coded wait

--- airbnb_multicalendar.user.js
+++ airbnb_multicalendar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Airbnb Multicalendar
 // @namespace    aljgom
-// @version      0.12
+// @version      0.121
 // @description  Adds guest photos to the bookings in the multicalendar
 //               Changes the background color of bookings that include today. Creates a set with all the guests for the current day
 // @author       aljgom
@@ -23,27 +23,28 @@
     let bookings = await waitFor(()=>document.getElementsByClassName('_1vd7r9f'))
     bookings[0].click();
     await waitFor(()=>document.querySelectorAll('._1okh7pi0 div._czm8crp')[0]) // wait for guest details to load (this is the selector for check-in)
-    log('bookings', bookings)
 
     let processBeds = async bookings =>{
-        log('starting')
         let runs = processBeds.runs = processBeds.runs ? processBeds.runs+1 : 1
-        log(runs)
-        let now = new Date();
-        let wait = 50;
+        let today = new Date();
+        today.setHours(0,0,0,0)
+        let itinURL                                     // full itinerary url
         for(let i=0; i<bookings.length; i++){
             if(runs != processBeds.runs) return;        // stop if another instance of this functions starts
-            let booking= bookings[i]
+            let booking = bookings[i]
             if(booking.processed) continue;
             booking.processed = true;
             booking.click()
-            await sleep(wait);
+            while(itinURL == document.querySelectorAll('._9m9ayv a')[0].href){  // "full itinerary" url
+                await sleep(10);
+            }
+            itinURL = document.querySelectorAll('._9m9ayv a')[0].href;
             // get checkin and checkout dates for other uses
             let checkin = document.querySelectorAll('._1okh7pi0 div._czm8crp')[0].innerHTML
-            checkin = new Date(checkin +' '+ (1900+ now.getYear()))
+            checkin = new Date(checkin +' '+ (1900+ today.getYear()))
             let checkout= document.querySelectorAll('._1okh7pi0 div._czm8crp')[1].innerHTML
-            checkout = new Date(checkout +' '+ (1900+ now.getYear()))
-            checkout.setDate(checkout.getDate()+1);
+            checkout = new Date(checkout +' '+ (1900+ today.getYear()))
+            //checkout.setDate(checkout.getDate()+1);
             let guest = document.querySelectorAll('._n5lh69r ._1p3joamp')[0]
             // get guest photo, clone it, style it
             let photo = document.querySelectorAll('._e296pg>img')[0]
@@ -52,20 +53,21 @@
             photo2.style.height = photo2.style.width = "25px"
             booking.children[1].append(photo2)
             // log if it's one of today's bookings
-            if(checkin <= now && now <= checkout ){
+            if(checkin <= today && today <= checkout ){
                 if(!guests.has(guest.innerText)){       // can be 2 bookings same guest, but add it to the cointainer only once
                     container.append(guest.cloneNode(true))
                     container.append(photo.cloneNode(true));
                 }
                 guests.add(guest.innerText)
-                booking.style.backgroundColor = 'gray'
-                log(booking)
-                log(checkin, checkout)
+                // log(booking)
+                // color bookings for today, highlight checkins and checkouts
+                booking.style.backgroundColor = checkin.valueOf() == today.valueOf() ? 'DarkViolet' : checkout.valueOf() == today.valueOf() ? 'DarkOrange' : 'gray';
+                //log(checkin, checkout)
             }
             await sleep(50);
         }
 
-        log(guests)
+      //  log(guests)
     };
     let check_beds = ()=>{
         // let bookings = await waitFor(()=>document.getElementsByClassName('_1vd7r9f'))
